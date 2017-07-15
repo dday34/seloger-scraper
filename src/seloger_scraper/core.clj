b5380b7021ac3ac55676232228e30927427b3513
Damien Sendner <damien.sendner@gmail.com>
2017-07-15 20:32:49 +0100

    scrape seloger results for specific city and price

--- src/seloger_scraper/core.clj
+++ src/seloger_scraper/core.clj
@@ -12,11 +12,20 @@
 (defn get-link [l]
   (-> l :attrs :href))
 
-(defn get-links [url]
+(defn get-listing-links [url]
   (map get-link (html/select (html/html-resource (:body (fetch-url url))) [:div.title :a])))
 
 (defn scrape-seloger-listings [url]
-  (map listing/scrape-listing (get-links url)))
+  (let [listing-links (get-listing-links url)]
+    (println "Start scraping " (count listing-links) " listings")
+    (map listing/scrape-listing listing-links)))
+
+(defn get-city-id [city]
+  (let [city-ids {"montpellier" "340172"}]
+      (get city-ids city)))
+
+(defn search [city max-price]
+  (scrape-seloger-listings (str "http://www.seloger.com/list.htm?tri=initial&idtypebien=2,1&idtt=2&ci=" (get-city-id city) "&pxmax=" max-price "&naturebien=1,2,4")))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- src/seloger_scraper/listing.clj
+++ src/seloger_scraper/listing.clj
@@ -3,6 +3,7 @@
    [sparkledriver.core :refer [with-browser make-browser fetch! find-by-id find-by-xpath* text find-by-class]]))
 
 (defn scrape-listing [url]
+  (println "scrape listing " url)
   (with-browser [listing-page (fetch! (make-browser) url)]
     {:title (-> listing-page (find-by-id "js-titleAnnonce") text)
      :description (-> listing-page (find-by-id "js-descriptifBien") text)}))
