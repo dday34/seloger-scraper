9a6a99d139696c21078aa2991fb2b8041f6fa074
Damien Sendner <damien.sendner@gmail.com>
2017-07-23 16:19:10 +0100

    added pagination to construct url function

--- src/seloger_scraper/core.clj
+++ src/seloger_scraper/core.clj
@@ -42,14 +42,16 @@
   (let [city-ids {"montpellier" "340172"}]
       (get city-ids city)))
 
-(defn construct-seloger-url [city max-price]
-  (str "http://www.seloger.com/list.htm?tri=initial&idtypebien=2,1&idtt=2&ci=" (get-city-id city) "&pxmax=" max-price "&naturebien=1,2,4"))
+(defn construct-seloger-url
+  ([city max-price] (construct-seloger-url city max-price 1))
+  ([city max-price page]
+   (str "http://www.seloger.com/list.htm?tri=initial&idtypebien=2,1&idtt=2&ci=" (get-city-id city) "&pxmax=" max-price "&naturebien=1,2,4&LISTING-LISTpg=" page)))
 
 (defn search [city max-price]
   (let [output-file-name (str (.format (java.text.SimpleDateFormat. "ddMMyyyy") (new java.util.Date)) "-" city "-" max-price "max.json")
         url (construct-seloger-url city max-price)]
     (for [page (range 1 (inc (get-number-of-pages url)))]
-      (scrape-seloger-listings (str url "&LISTING-LISTpg=" page) output-file-name))))
+      (scrape-seloger-listings (construct-seloger-url city max-price page) output-file-name))))
 
 (defn -main
   "I don't do a whole lot ... yet."
