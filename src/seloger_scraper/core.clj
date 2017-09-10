f7532cfa8521fca3697fa05ac923354a8c5100de
Damien Sendner <damien.sendner@gmail.com>
2017-09-10 19:02:22 +0100

    added help

--- src/seloger_scraper/core.clj
+++ src/seloger_scraper/core.clj
@@ -2,26 +2,31 @@
   (:require
    [clojure.string :as string]
    [clojure.tools.cli :refer [parse-opts]]
-   [clojure.tools.logging :as log]
    [seloger-scraper.listings :as listings])
   (:gen-class))
 
 (def cli-options
-  [["-c" "--city" "Name of the city you want to search listings for."
+  [["-c" "--city" "Name of the city you want to search accommodation in."
     :required true
     :default "montpellier"
     :parse-fn str
     :validate [listings/has-city? (str "Must be one of these cities: " (string/join "," (listings/get-cities)))]]
-   ["-m" "--max-price" "The Maximum price for the listings."
+   ["-m" "--max-price" "The Maximum selling price."
     :required true
     :default 50000
     :parse-fn #(Integer/parseInt %)
-    :validate [#(< 0 %) "Must be greater than zero."]]])
+    :validate [#(< 0 %) "Must be greater than zero."]]
+   ["-h" "--help"]])
 
 (defn -main
   [& args]
-  (let [{{city :city max-price :max-price} :options errors :errors} (parse-opts args cli-options)]
-    (if errors
-      (log/info errors)
-      (listings/search city max-price))
+  (let [{{city :city max-price :max-price help :help} :options errors :errors} (parse-opts args cli-options)]
+    (cond
+      errors (println errors)
+      help (println (string/join "\n" (cons
+                                       "Scrape accomodation listings on seloger.com"
+                                       (map #(->> %
+                                                  (take 3)
+                                                  (string/join " ")) cli-options))))
+      :else (listings/search city max-price))
     (System/exit 0)))
